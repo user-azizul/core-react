b1af304d687669f4d50615f97aed92cdd23e5deb
user-azizul <azizul.contact.me@gmail.com>
2024-06-19 22:58:23 +0300

    conditional rendering in react

--- basic-react/src/PropsComp.jsx
+++ basic-react/src/PropsComp.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Car(props) {
+// function Car(props) {
+//   return (
+//     <h2>
+//       I am a {props.brand.car} of model {props.brand.model}!
+//     </h2>
+//   );
+// }
+
+// function Garage() {
+//   const carInfo = { car: "bm", model: 2023 };
+//   return (
+//     <div>
+//       <h2>Who lives in the garage?</h2>
+//       <Car brand={carInfo} />
+//     </div>
+//   );
+// }
+
+/// conditional rendering ********************
+
+function Car(cars) {
   return (
-    <h2>
-      I am a {props.brand.car} of model {props.brand.model}!
-    </h2>
+    <>
+      <h1> Garage </h1>
+      {cars.length > 0 && <h2>You have {cars.length} cars in your garage.</h2>}
+    </>
   );
 }
-
-function Garage() {
-  const carInfo = { car: "bm", model: 2023 };
-  return (
-    <div>
-      <h2>Who lives in the garage?</h2>
-      <Car brand={carInfo} />
-    </div>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(<Garage />);
+const cars = [1, 2, 3];
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Car cars={cars} />
+);
